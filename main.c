0dcb4222165bf2c0ef752753dd7a86547b7c6557
mondoman712 <oommmmmmmo@googlemail.com>
2015-10-31 15:41:32 +0000

    creates window

--- main.c
+++ main.c
@@ -3,12 +3,27 @@
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 
+#define WIN_TITLE "window title"
+#define DEFAULT_SCREEN_X 640
+#define DEFAULT_SCREEN_Y 480
+
 int main()
 {
 	if (SDL_Init(SDL_INIT_VIDEO)) {
 		fprintf(stderr, "Failed to initialise SDL\n");
 		exit(EXIT_FAILURE);
 	}
+
+	SDL_Window * mainwin;
+	mainwin = SDL_CreateWindow(
+			WIN_TITLE,
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			DEFAULT_SCREEN_X,
+			DEFAULT_SCREEN_Y,
+			SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+
 
 	return 0;
 }
