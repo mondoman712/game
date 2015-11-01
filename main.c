4b99852cb92a61c42f46722131045626e2330545
mondoman712 <oommmmmmmo@googlemail.com>
2015-11-01 23:29:54 +0000

    found the shaders wernt reading, fixed and my triangle is now drawn

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 
 GLuint create_shader (GLenum shader_type, char * filename)
 {
-	GLchar * _source= malloc(4096);
+	char _source[4096];
 	char dest[64];
 
 	strcpy(dest, SHADER_DIR);
@@ -25,17 +25,32 @@ GLuint create_shader (GLenum shader_type, char * filename)
 		fprintf(stderr, "Couldn't read %s\n", filename);
 		return 1;
 	}
-	fread(_source, 512, 8, s);
-	fclose(s);
+	
+	int i = 0;
+	char inc;
+	while (fscanf(s, "%c", &inc) >0)
+		_source[i++] = inc;
+	
+	_source[i - 1] = '\0';
 
-	const GLchar * source = _source;
-	free(_source);
+	const char * source = _source;
+	fclose(s);
 
 	GLuint shader = glCreateShader(shader_type);
 	glShaderSource(shader, 1, &source, NULL);
 	glCompileShader(shader);
 
-	printf("Shader %s has been compiled\n", filename);
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if (status == GL_TRUE) {
+		printf("Shader %s has been compiled\n", filename);
+	} else {
+		char buff[512];
+		glGetShaderInfoLog(shader, 512, NULL, buff);
+		fprintf(stderr, "Shader %s failed to compile with error:\n", filename);
+		fprintf(stderr, "%s\n", buff);
+		fprintf(stderr, "\n");
+	}
 
 	return shader;
 }
--- shaders/fs1.glsl
+++ shaders/fs1.glsl
@@ -2,7 +2,7 @@
 
 out vec4 out_colour;
 
-void main ()
+void main()
 {
 	out_colour = vec4(1.0, 1.0, 1.0, 1.0);
 }
