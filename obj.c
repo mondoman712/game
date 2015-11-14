2407c3dc655981f2653c898e76adf4e2fed4a774
mondoman712 <oommmmmmmo@googlemail.com>
2015-11-14 19:23:03 +0000

    fixed reading faces in obj.c, counts for faces and vertices now stored in first value in array

--- obj.c
+++ obj.c
@@ -20,7 +20,7 @@ static int parse_vector (char * str, GLfloat * buff)
 	strtok(_str, " ");
 
 	while ((wd = strtok(NULL, " "))) {
-		buff[i] = atof(wd);
+		buff[i] = (GLfloat) atof(wd);
 		i++;
 	}
 
@@ -44,7 +44,7 @@ static int parse_face (char * str, GLuint * buff)
 	strtok(_str, " ");
 
 	while ((wd = strtok(NULL, " "))) {
-		buff[i] = atoi(wd) - 1;
+		buff[i] = (GLuint) (atoi(wd) - 1);
 		i++;
 	}
 
@@ -58,13 +58,10 @@ cleanup:
 /* 
  * Reads obj file and deposits vertices into float array
  */
-size_t read_obj (const char * filename, GLfloat * vertices, GLuint * faces,
-		GLuint * facec)
+int read_obj (const char * filename, GLfloat * vertices, GLuint * faces)
 {
-	char buf[80];
-	int vertc = 0;
-	GLfloat * _verts = vertices;
-	GLuint * _faces = faces;
+	char buf[64];
+	GLuint vc = 0;
 	GLuint fc = 0;
 
 	FILE * m = fopen(filename, "r");
@@ -72,40 +69,54 @@ size_t read_obj (const char * filename, GLfloat * vertices, GLuint * faces,
 		fprintf(stderr, "Could not open file %s\n", filename);
 		return 1;
 	}
-
+	
 	while (fgets(buf, sizeof(buf), m)) {
 		if (buf[0] == 'v' && buf[1] == ' ') {
-			parse_vector(buf, _verts +(vertc * 3));
-			vertc++;
-		} else if (buf[0] == 'f') {
-			parse_face(buf, _faces + (fc * 3));
+			parse_vector(buf, vertices + (vc * 3) + 1);
+			vc++;
+		} else if (buf[0] == 'f' && buf[1] == ' ') {
+			parse_face(buf, faces + (fc * 3) + 1);
 			fc++;
 		}
-
 	}
 
 	fclose(m);
 
-	return vertc;
+	*vertices = (GLfloat) vc;
+	*faces = fc;
+
+	return 0;
 }
 
 int main ()
 {
-	size_t vertlen = 0;
-	GLuint * facec = 0;
 	int i;
 
 	GLfloat * vertices = malloc(128 * sizeof(GLfloat));
 	GLuint * faces = malloc(128 * sizeof(GLuint));
-	vertlen = read_obj("models/sphere.obj", vertices, faces, facec);
+	if (vertices == NULL || faces == NULL) {
+		fprintf(stderr, "Failed to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
+	read_obj("models/cube.obj", vertices, faces);
 
-	printf("%i\n", (int) vertlen);
+	printf("vertices: %i\n", (int) *vertices);
+	printf("faces: %d\n", *faces);
 
-	for (i = 0; i < (int) (vertlen * 3); i += 3) {
+	for (i = 1; i < (int) (*vertices * 3); i += 3) {
 		printf("%f, ", *(vertices + i));
 		printf("%f, ", *(vertices + i + 1));
 		printf("%f\n", *(vertices + i + 2));
 	}
+
+	for (i = 1; i < (int) (*faces * 3); i += 3) {
+		printf("%d, ", *(faces + i));
+		printf("%d, ", *(faces + i + 1));
+		printf("%d\n", *(faces + i + 2));
+	}
+
+	free(vertices);
+	free(faces);
 
 	exit(EXIT_SUCCESS);
 }
