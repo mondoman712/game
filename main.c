22a52194d15d6bad34b04c728fe713c451209d6c
mondoman712 <oommmmmmmo@googlemail.com>
2015-11-16 12:53:07 +0000

    going to try from the start again to see if I can fix some of these issues

--- main.c
+++ main.c
@@ -5,14 +5,9 @@
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 
-#include "obj.h"
-#include "trans.h"
-
 #define WIN_TITLE "window title"
 #define DEFAULT_SCREEN_X 1280
 #define DEFAULT_SCREEN_Y 720
-
-#define PI 3.141592653589
 
 #define GL_MAJOR_VER 3
 #define GL_MINOR_VER 3
@@ -112,153 +107,6 @@ int main()
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, GL_MAJOR_VER);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, GL_MINOR_VER);
 
-	glewExperimental = GL_TRUE;
-	GLenum glew_status = glewInit();
-	if (glew_status) {
-		fprintf(stderr, "Error %s\n", glewGetErrorString(glew_status));
-		exit(EXIT_FAILURE);
-	} else {
-		printf("GLEW is working\n");
-	}
-
-	GLfloat vertices[] = {
-		8.0,
-		 0.5, -0.5, -0.5,
-		 0.5, -0.5,  0.5,
-		-0.5, -0.5,  0.5,
-		-0.5, -0.5, -0.5,
-		 0.5,  0.5, -0.5,
-		 0.5,  0.5,  0.5,
-		-0.5,  0.5,  0.5,
-		-0.5,  0.5, -0.5
-	};
-
-	GLuint faces[] = {
-		12,
-		1, 2, 3,
-		7, 6, 5,
-		4, 5, 1,
-		5, 6, 2,
-		2, 6, 7,
-		0, 3, 7,
-		0, 1, 3,
-		4, 7, 5,
-		0, 4, 1,
-		1, 5, 2,
-		3, 2, 7,
-		4, 0, 7
-	};
-
-	/*
-	GLfloat * vertices = malloc(16 * sizeof(GLfloat));
-	GLuint * faces = malloc(16 * sizeof(GLuint));
-	if (vertices == NULL || faces == NULL) {
-		fprintf(stderr, "Failed to allocate memory 01\n");
-		exit(EXIT_FAILURE);
-	}
-	read_obj("models/cube.obj", vertices, faces);
-
-	int i;
-	printf("vertices: %i\n", (int) *vertices);
-	for (i = 1; i < (*vertices * 3); i += 3) {
-		printf("%f, ", *(vertices + i));
-		printf("%f, ", *(vertices + i + 1));
-		printf("%f\n", *(vertices + i + 2));
-	}
-
-	printf("faces: %i\n", (int) *faces);
-	for (i = 1; i < (int) (*faces * 3); i += 3) {
-		printf("%d, ", *(faces + i));
-		printf("%d, ", *(faces + i + 1));
-		printf("%d\n", *(faces + i + 2));
-	}
-	*/
-
-	GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 
-			(*vertices * 3 * sizeof(GLfloat)),
-			vertices + 1,
-			GL_STATIC_DRAW);
-
-	GLuint vertex_shader = create_shader(GL_VERTEX_SHADER, "vs1");
-	GLuint fragment_shader = create_shader(GL_FRAGMENT_SHADER, "fs1");
-
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	GLuint ebo;
-	glGenBuffers(1, &ebo);
-
-	GLuint shader_program = glCreateProgram();
-	glAttachShader(shader_program, vertex_shader);
-	glAttachShader(shader_program, fragment_shader);
-	glBindFragDataLocation(shader_program, 0, "out_colour");
-
-	glLinkProgram(shader_program);
-	glUseProgram(shader_program);
-
-	GLint pos_attrib = glGetAttribLocation(shader_program, "position");
-	glEnableVertexAttribArray(pos_attrib);
-	glVertexAttribPointer(pos_attrib, 3, GL_FLOAT, GL_FALSE, 
-			3 * sizeof(GLfloat), 0);
-
-	GLfloat view[16];
-	vec3 eye = {8.0, 8.0, 8.0};
-	vec3 cent = {0.0, 0.0, 0.0};
-	vec3 up = {0.0, 0.0, 1.0};
-	look_at(eye, cent, up, view);
-	GLint uni_view = glGetUniformLocation(shader_program, "view");
-	glUniformMatrix4fv(uni_view, 1, GL_FALSE, view);
-
-	GLfloat proj[16];
-	perspective(PI / 2, DEFAULT_SCREEN_X / DEFAULT_SCREEN_Y, 1.0, 10.0,
-			proj);
-	GLint uni_proj = glGetUniformLocation(shader_program, "proj");
-	glUniformMatrix4fv(uni_proj, 1, GL_FALSE, proj);
-
-	GLfloat model[16];
-	GLint uni_model = glGetUniformLocation(shader_program, "model");
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-			*faces, (faces + 1), GL_STATIC_DRAW);
-
-	glEnable(GL_DEPTH_TEST);
-
-	float k = 0.0;
-	SDL_Event e;
-	while (1) {
-		if (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT) 
-				break;
-			else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_q)
-				break;
-		}
-		glClearColor(0.0, 0.0, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		
-		rotatez((PI / 180) * (k += 0.002), model);
-		glUniformMatrix4fv(uni_model, 1, GL_FALSE, model);
-
-		glDrawElements(GL_TRIANGLES, (*faces * 3), GL_UNSIGNED_INT, 0);
-
-		SDL_GL_SwapWindow(mainwin);
-	}
-
-	/*
-	free(vertices);
-	free(faces);
-	*/
-
-	glDeleteProgram(shader_program);
-	glDeleteShader(fragment_shader);
-	glDeleteShader(vertex_shader);
-
-	glDeleteBuffers(1, &vbo);
-	glDeleteVertexArrays(1, &vao);
 
 	SDL_GL_DeleteContext(gl_context);
 	SDL_DestroyWindow(mainwin);
