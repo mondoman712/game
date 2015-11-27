2a97d381eb27cb976e2d8fddc1af8320c5502331
mondoman712 <oommmmmmmo@googlemail.com>
2015-11-27 14:29:30 +0000

    working on obj reading in scheme

--- src/f_obj.scm
+++ src/f_obj.scm
@@ -1,13 +1,17 @@
 (define (string->numlist str)
   (map string->number (string-split str #\space)))
 
-(define (read-obj-lines file)
+(define (read-obj-lines file lst)
   (let ((line (read-line file)))
-    (unless (eof-object? line)
-      (display line)
-      (newline)
-      (read-obj-lines file))))
+    (if (eof-object? line)
+	(if (string-contains line "v " 0 2)
+	    (begin
+	      (display line)
+	      (newline)
+	      (read-obj-lines file (append lst (string->numlist (substring line 2)))))
+	    (read-obj-lines file lst))
+	lst)))
 
 (define (load-obj filename)
   (let ((fp  (open-file filename "r")))
-    (read-obj-lines fp)))
+    (read-obj-lines fp '())))
